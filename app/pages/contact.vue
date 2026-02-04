d55c7ac4c3a87d9f2dfe42373534caec0cc7d61b
shehbazasif <m.shahbazasif512@gmail.com>
2026-02-04 18:11:31 +0300

    Change Dummy Website Link to Real Website

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -46,8 +46,8 @@ const contactInfo = [
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
     </svg>`,
     label: "Website",
-    value: "www.maya.intl.com",
-    note: "(under development)"
+    value: "www.mayassc.com",
+    href: "https://www.mayassc.com"
   }
 ]
 </script>
